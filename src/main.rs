4a0fe60c6069cded9a86155933bbf69dfee2c3f6
Heverton <hb.hevertonborges@gmail.com>
2024-02-21 19:26:35 -0300

    almost there

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
 use std::io;
 
 fn main() {
-    let mut binary_number: String = String::new();
+    let mut binary_string: String = String::new();
 
     println!("Enter a number to be convert: ");
 
     io::stdin()
-        .read_line(&mut binary_number)
+        .read_line(&mut binary_string)
         .expect("Failed to read line");
 
-    let mut length: usize = binary_number.len();
+    binary_string = binary_string.chars().rev().collect();
 
-    binary_number[2]
+    let mut decimal_number: i64 = 0;
+    let mut index: i64 = 0;
+    
+    for i in binary_string.chars() {
+        if i == '0' {
+            index += 1  
+        }
+        if i == '1' {
+            decimal_number += result(index);
+        }
+    }
 
-    // while length != 0 {
+    println!("The number converted is {}", decimal_number);
+}
 
-    //     length -=1
-    // }
+fn result(i: i64) -> i64 {
+    let mut id = i;
+    let mut res = 1;
+
+    while id != 0 {
+        res *= 2;
+        id -= 1;
+    }
+    return res;
 }
